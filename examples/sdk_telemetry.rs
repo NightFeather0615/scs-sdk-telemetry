185a7f86514d6702c039473ad485ad12cf10e5b4
NightFeather <leolee50910@gmail.com>
2023-09-09 14:41:31 +0800

    add example

---
+++ examples/sdk_telemetry.rs
@@ -0,0 +1,12 @@
+use scs_sdk_telemetry::shared_memory::SharedMemory;
+
+fn main() {
+  let mut shared_mem: SharedMemory = SharedMemory::connect();
+
+  println!("{:#?}", shared_mem.read());
+  
+  #[cfg(feature = "json")]
+  println!("{:#?}", shared_mem.read().to_json().unwrap().to_string());
+
+  shared_mem.dispose();
+}
